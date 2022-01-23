1da3ff51106fc6ff26e67ea8192d4452218bb536
BuildTools <nicthomasnnt@gmail.com>
2022-01-23 16:35:15 -0500

    fetch upstream + fix

---
+++ settings.js
@@ -0,0 +1,34 @@
+const fonts = [
+    'BalooTammudu',
+    'SegoeUI',
+    'Roboto',
+    'Lato',
+    'NotoSans',
+    'SourceCodePro',
+    'Impact',
+    'Comfortaa',
+    'DancingScript',
+    'IndieFlower',
+    'PressStart2P',
+    'Wallpoet',
+    'Minecraft'
+];
+
+const sizes = [
+    'small',
+    'medium',
+    'large'
+];
+
+const strokes = [
+    'thin',
+    'medium',
+    'thick',
+    'thicker'
+];
+
+const shadows = [
+    'small',
+    'medium',
+    'large'
+];
